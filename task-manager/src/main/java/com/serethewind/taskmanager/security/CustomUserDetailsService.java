a5b935ff5d9dda135a652c4080f79a499c777225
serethewind <osasereu@gmail.com>
2023-05-22 06:46:10 +0100

    Database authentication fully implemented

--- task-manager/src/main/java/com/serethewind/taskmanager/security/CustomUserDetailsService.java
+++ task-manager/src/main/java/com/serethewind/taskmanager/security/CustomUserDetailsService.java
@@ -3,18 +3,31 @@ package com.serethewind.taskmanager.security;
 import com.serethewind.taskmanager.entity.User;
 import com.serethewind.taskmanager.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, sernameOrEmail).orElseThrow(() ->);
-        return
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException("User with ID not found"));
+
+        Set<GrantedAuthority> grantedAuthorities = user.getRoles().stream().map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                usernameOrEmail,
+                user.getPassword(),
+                grantedAuthorities
+        );
     }
 }
